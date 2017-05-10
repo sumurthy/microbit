c1c6eeeeaea946cc24fa27c29c903eb44022cc2d
sumurthy <rsudhi@gmail.com>
2017-05-09 22:41:39 -0700

    Saving... code is not working

--- src/App.js
+++ src/App.js
@@ -9,22 +9,29 @@ class App extends Component {
 
 
   handleClick() {
-    navigator.bluetooth.requestDevice({ filters: [{ services: [0xFFA0] }] })
-    .then(device => device.gatt.connect())
+    navigator.bluetooth.requestDevice({  
+      filters: [
+        {name: 'BBC micro:bit'},
+        {services: ['e95d0753-251d-470a-a062-fa1922dfa9a8']}  //ACCELEROMETER SERVICE
+      ]
+    })    
+    .then(device => device.gatt.connect())   
     .then(server => {
       // Getting Service...
-      return server.getPrimaryService(0xFFA0);
+      console.log('Connected to Microbit: ' + server )
+      return server.getPrimaryService('e95d0753-251d-470a-a062-fa1922dfa9a8'); //ACCELEROMETER SERVICE
     })
     .then(service => {
       // Getting Characteristic...
-      return service.getCharacteristic(0xFFA0);
+      //ACCELEROMETER SERVICE - CHARACTERISTICS: Accelerometer Data
+      return service.getCharacteristic('e95dca4b-251d-470a-a062-fa19-22dfa9a8'); 
     })
     .then(characteristic => {
       // Reading  Level...
       return characteristic.readValue();
     })
     .then(value => {
-      console.log('Battery percentage is ' + value.getUint8(0));
+      console.log('Value is ' + value.getUint8(0));
     })
     .catch(error => { console.log('Error: ' + error); });
   }
@@ -34,7 +41,7 @@ class App extends Component {
     return (
       <div >
         <div>
-          <h2>Welcome to MicroBit Excel Demo</h2>
+          <h2>Welcome to MicroBit Demo</h2>
         </div>
         <p>
           To get started, <code><button onClick={this.handleClick}> click here</button></code> and pair the device.
